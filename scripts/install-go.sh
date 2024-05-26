8f12468ad66c727d5f45c62fdf2b00e4318027a2
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-05-26 12:00:01 -0400

    auto plugin setup 1

--- scripts/install-go.sh
+++ scripts/install-go.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo wget https://go.dev/dl/go1.21.4.linux-amd64.tar.gz -O go.tar.gz
 sudo tar -xzvf go.tar.gz -C /usr/local
 echo export PATH=$HOME/go/bin:/usr/local/go/bin:$PATH >> ~/.profile
