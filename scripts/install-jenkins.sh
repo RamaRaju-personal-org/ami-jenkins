0875eaa129cd9ca01208314cc15d39dbd2057939
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-05-26 00:58:11 -0400

    commit 11

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Set non-interactive frontend
+export DEBIAN_FRONTEND=noninteractive
+
 # Update package list and install OpenJDK
 sudo apt-get update -y
 sudo apt-get install -y openjdk-11-jdk
 
 # Add Jenkins key and repository
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # Update package list and install Jenkins
 sudo apt-get update -y
