edb5eb5da061bdcc5900224e2bff9229f072bdb6
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-05-26 02:30:13 -0400

    commit 12

--- scripts/install-caddy.sh
+++ scripts/install-caddy.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+echo "|                                                                                                                                         |"
+echo "|                                                               INSTALL CADDY                                                             |"
+echo "|                                                                                                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 
-# Install dependencies
+# Caddy(stable) installation docs: https://caddyserver.com/docs/install#debian-ubuntu-raspbian
+
+# Install and configure keyring for caddy stable release:
 sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo \
+  gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee \
+  /etc/apt/sources.list.d/caddy-stable.list
 
-# Add Caddy GPG key and repository
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list
+# Install caddy:
+sudo apt-get update && sudo apt-get install caddy -y
 
-# Update package list and install Caddy
-sudo apt-get update -y
-sudo apt-get install -y caddy
+# Check the status of Caddy service
+sudo systemctl --full status caddy
+
+# Check Caddy version
+echo "Caddy $(caddy --version)"
 
 # Enable Caddy service
 sudo systemctl enable caddy
--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -2,21 +2,51 @@
 
 # Set non-interactive frontend
 export DEBIAN_FRONTEND=noninteractive
+export CHECKPOINT_DISABLE=1
 
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+echo "|                                                                                                                                         |"
+echo "|                                                               INSTALL JAVA 11                                                           |"
+echo "|                                                                                                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 # Update package list and install OpenJDK
-sudo apt-get update -y
 sudo apt-get install -y openjdk-11-jdk
+sleep 3
+echo "Java $(java -version)"
 
+
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+echo "|                                                                                                                                         |"
+echo "|                                                                INSTALL JENKINS                                                          |"
+echo "|                                                                                                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 # Add Jenkins key and repository
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+# Jenkins setup on Debian (stable): https://pkg.jenkins.io/debian-stable/
+
+# Debian package repository of Jenkins to automate installation and upgrade.
+# To use this repository, first add the key to the system:
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc >/dev/null
+
+# Add a Jenkins apt repository entry:
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  /etc/apt/sources.list.d/jenkins.list >/dev/null
 
-# Update package list and install Jenkins
-sudo apt-get update -y
-sudo apt-get install -y jenkins
+# Install Jenkins:
+sudo apt-get update && sudo apt-get install jenkins -y
+
+sleep 3
+
+sudo systemctl start jenkins
 
 # Enable Jenkins service
 sudo systemctl enable jenkins
+sleep 3
+
+# Check the status of Jenkins service
+sudo systemctl --full status jenkins
+
+# Check Jenkins version
+echo "Jenkins $(jenkins --version)"
