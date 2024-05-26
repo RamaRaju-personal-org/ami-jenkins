174b0dc80de7e11a3de098db0365df90d97e8018
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-05-26 03:58:04 -0400

    packer update again2

--- scripts/jenkins-install.sh
+++ scripts/jenkins-install.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-sudo apt install openjdk-11-jdk -y 
-
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add - 
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
-
-sudo apt update && sudo apt install jenkins -y
-
-sudo systemctl start jenkins
-
-sleep 5 
-
+sudo apt-get update -y
+sudo apt-get install openjdk-11-jdk -y
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
 sudo systemctl enable jenkins
