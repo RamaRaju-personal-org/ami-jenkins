9ccb6e95015f9522c33b4e7b81d7148f0eeb1646
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-05-26 11:29:21 -0400

    upgrades

---
+++ scripts/Jenkins-AutoPlugin-SetUp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# Install jenkins-plugin-manager
+echo "Installing jenkins-plugin-manager..."
+wget --quiet https://github.com/jenkinsci/plugin-installation-manager-tool/releases/download/2.12.13/jenkins-plugin-manager-2.12.13.jar
+
+
+# Install plugins using jenkins-plugin-manager tool
+echo "Installing plugins..."
+sudo java -jar ./jenkins-plugin-manager-2.12.13.jar --war /usr/share/java/jenkins.war \
+  --plugin-download-directory /var/lib/jenkins/plugins --plugin-file plugins.txt
+
+
+# Restart Jenkins to apply changes
+echo "Restarting Jenkins..."
+systemctl restart jenkins
+
+echo "Jenkins setup is complete with recommended plugins installed, permissions updated, and setup wizard disabled."
