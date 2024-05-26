c79e183abb49dad64b7fb12d26df810d70faf097
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-05-26 02:47:06 -0400

    commit 13

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Set non-interactive frontend
-export DEBIAN_FRONTEND=noninteractive
-export CHECKPOINT_DISABLE=1
 
 echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
 echo "|                                                                                                                                         |"
@@ -50,3 +47,50 @@ sudo systemctl --full status jenkins
 
 # Check Jenkins version
 echo "Jenkins $(jenkins --version)"
+
+
+
+
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+echo "|                                                                                                                                         |"
+echo "|                                                               INSTALL CADDY                                                             |"
+echo "|                                                                                                                                         |"
+echo "+-----------------------------------------------------------------------------------------------------------------------------------------+"
+
+# Caddy(stable) installation docs: https://caddyserver.com/docs/install#debian-ubuntu-raspbian
+
+# Install and configure keyring for caddy stable release:
+sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo \
+  gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee \
+  /etc/apt/sources.list.d/caddy-stable.list
+
+# Install caddy:
+sudo apt-get update && sudo apt-get install caddy -y
+
+# Check the status of Caddy service
+sudo systemctl --full status caddy
+
+# Check Caddy version
+echo "Caddy $(caddy --version)"
+
+# Enable Caddy service
+sudo systemctl enable caddy
+
+# Remove default Caddyfile
+sudo rm /etc/caddy/Caddyfile
+
+# Create new Caddyfile for Jenkins
+sudo tee /etc/caddy/Caddyfile <<EOF
+jenkins.ramaraju.cloud {
+  reverse_proxy 127.0.0.1:8080
+  tls jenkins.ramaraju.cloud
+}
+EOF
+
+# Restart Caddy service to apply new configuration
+sudo systemctl restart caddy
+
+# Enable Caddy service
+sudo systemctl enable caddy
