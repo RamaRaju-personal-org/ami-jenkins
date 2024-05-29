159a5da6258c5449559c8d7199c326e1041a6291
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-05-28 21:01:03 -0400

    new dd

--- scripts/Jenkins-AutoPlugin-SetUp.sh
+++ scripts/Jenkins-AutoPlugin-SetUp.sh
@@ -58,31 +58,20 @@ echo "Installing plugins..."
 sudo java -jar ./jenkins-plugin-manager-2.12.13.jar --war /usr/share/java/jenkins.war \
   --plugin-download-directory /var/lib/jenkins/plugins --plugin-file /tmp/plugins.txt
 
-# Restart Jenkins
-sudo systemctl restart jenkins
 echo "Plugins installed successfully."
 
 # Move JCasC YAML and Groovy script files
 echo "Moving files"
-sudo mv /home/ubuntu/Jcasc.yml  /var/lib/jenkins/Jcasc.yml
-sudo mv /home/ubuntu/jenkins-setup.groovy /var/lib/jenkins/jenkins-setup.groovy
+sudo mv /home/ubuntu/jcasc.yml /var/lib/jenkins/Jcasc.yml
+sudo mv /home/ubuntu/jenkins-UserSetUp.groovy /var/lib/jenkins/jenkins-setup.groovy
 
 # Update file ownership
 echo "Updating file ownership"
 cd /var/lib/jenkins/ || exit
 sudo chown jenkins:jenkins ./Jcasc.yml ./*.groovy
 
-# Configure JAVA_OPTS to disable setup wizard and apply JCasC configuration
-echo "Configuring JAVA_OPTS to disable setup wizard and apply JCasC configuration"
-sudo mkdir -p /etc/systemd/system/jenkins.service.d/
-{
-  echo "[Service]"
-  echo "Environment=\"JAVA_OPTS=-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false -Dcasc.jenkins.config=/var/lib/jenkins/Jcasc.yml\""
-} | sudo tee /etc/systemd/system/jenkins.service.d/override.conf
-
-# Restart Jenkins to apply JCasC configuration
-echo "Restarting Jenkins service to apply JCasC configuration"
-sudo systemctl daemon-reload
+# Restart Jenkins to apply plugin installation
+echo "Restarting Jenkins service to apply plugins"
 sudo systemctl restart jenkins
 sleep 60  # Wait for Jenkins to fully restart
 
@@ -90,61 +79,20 @@ sleep 60  # Wait for Jenkins to fully restart
 echo "Downloading Jenkins CLI..."
 wget -q http://localhost:8080/jnlpJars/jenkins-cli.jar -P /home/ubuntu/
 
-# # Run the Groovy script to create user and bypass setup wizard
-# echo "Running Groovy script to create user and bypass setup wizard"
-# sudo java -jar /home/ubuntu/jenkins-cli.jar -auth admin:admin -s http://localhost:8080/ groovy = /var/lib/jenkins/jenkins-setup.groovy
+# Run the Groovy script to create the user
+echo "Running Groovy script to create user"
+java -jar /home/ubuntu/jenkins-cli.jar -auth admin:admin -s http://localhost:8080/ groovy = /var/lib/jenkins/jenkins-setup.groovy
 
-# # Restart Jenkins service again to ensure changes are applied
-# echo "Restarting Jenkins service again to ensure changes are applied"
-# sudo systemctl restart jenkins
-# sudo systemctl enable jenkins
+# Disable the setup wizard by applying JCasC
+echo "Configuring JAVA_OPTS to disable setup wizard and apply JCasC configuration"
+sudo mkdir -p /etc/systemd/system/jenkins.service.d/
+{
+  echo "[Service]"
+  echo "Environment=\"JAVA_OPTS=-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false -Dcasc.jenkins.config=/var/lib/jenkins/jcasc.yml\""
+} | sudo tee /etc/systemd/system/jenkins.service.d/override.conf
 
-
-# Create seed job to run the Groovy script
-echo "Creating seed job to run Groovy script"
-cat <<EOF > /var/lib/jenkins/jobs/Run Groovy Script/config.xml
-<?xml version='1.1' encoding='UTF-8'?>
-<flow-definition plugin="workflow-job@2.39">
-  <actions>
-    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin="pipeline-model-definition@1.3.8"/>
-    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin="pipeline-model-definition@1.3.8">
-      <jobProperties/>
-      <triggers/>
-      <parameters/>
-      <options/>
-    </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>
-  </actions>
-  <description></description>
-  <keepDependencies>false</keepDependencies>
-  <properties/>
-  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2.80">
-    <script>
-    pipeline {
-      agent any
-      stages {
-        stage('Run Groovy Script') {
-          steps {
-            script {
-              def groovyScript = new File('/var/lib/jenkins/jenkins-setup.groovy')
-              evaluate(groovyScript)
-            }
-          }
-        }
-      }
-    }
-    </script>
-    <sandbox>true</sandbox>
-  </definition>
-  <triggers/>
-  <disabled>false</disabled>
-</flow-definition>
-EOF
-
-# Trigger the seed job
-echo "Triggering the seed job"
-java -jar /home/ubuntu/jenkins-cli.jar -auth admin:admin -s http://localhost:8080/ build "Run Groovy Script"
-
-# Restart Jenkins service again to ensure changes are applied
-echo "Restarting Jenkins service again to ensure changes are applied"
+# Restart Jenkins to apply JCasC configuration
+echo "Restarting Jenkins service to apply JCasC configuration"
+sudo systemctl daemon-reload
 sudo systemctl restart jenkins
 sudo systemctl enable jenkins
