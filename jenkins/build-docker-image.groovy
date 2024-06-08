602af33e37b9ce47113692b0e72884d82ac4f825
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-06-08 17:35:46 -0400

    update: jenkins DSL pipeline for docker image build 21

--- jenkins/build-docker-image.groovy
+++ jenkins/build-docker-image.groovy
@@ -1,51 +1,16 @@
-pipelineJob('StaticSitePipeline') {
+pipelineJob('docker-build-and-push') {
     definition {
-        cps {
-            script('''
-pipeline {
-    agent any 
-    environment {
-        GIT_REPO = 'git_static_site_repo_url'
-        GIT_BRANCH = 'main'
-        GIT_CREDENTIALS_ID = 'github-credentials-id'
-        DOCKER_HUB_CREDENTIALS = credentials('docker-hub-credentials-id')
-    }
-    stages {
-        stage("Clean Workspace") {
-            steps {
-                cleanWs()
-            }
-        }
-
-        stage("clone static-site repo") {
-            steps {
-                echo "cloning the repo"
-                withCredentials([string(credentialsId: "${GIT_CREDENTIALS_ID}", variable: 'GIT_TOKEN')]) {
-                    sh '''
-                        git config --global credential.helper cache
-                        git config --global url.https://${GIT_TOKEN}@github.com/.insteadOf https://github.com/
-                        git clone --branch ${GIT_BRANCH} ${GIT_REPO}
-                    '''
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('jenkins_ami_git_url')  // Replace with your GitHub repository URL
+                        credentials('github-credentials-id')
+                    }
+                    branch('*/main')
                 }
             }
-        }
-        
-        stage("build the caddy image and push") {
-            steps {
-                echo "building the image"
-                withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                    sh '''
-                        docker build -t ram2305/caddy:3.0 -f static-site/Dockerfile static-site
-                        docker login -u $USER -p $PASS
-                        docker push ram2305/caddy:3.0
-                    '''
-                }
-            }
-        }
-    }
-}
-            ''')
-            sandbox()
+            scriptPath('jenkins/Jenkinsfile')
         }
     }
 }
