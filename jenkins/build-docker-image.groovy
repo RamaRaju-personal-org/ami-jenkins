60da34496f588d3f31c9275ce80170c088f4def9
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-06-12 12:55:27 -0400

    feat(run groovy): execute in same script

--- jenkins/build-docker-image.groovy
+++ jenkins/build-docker-image.groovy
@@ -1,16 +1,60 @@
-pipelineJob('docker-build-and-push') {
+pipelineJob('StaticSitePipeline') {
+    description('Pipeline job to build and push Docker image for static-site')
+    triggers {
+        scm('* * * * *') // Polling the SCM every minute (adjust as needed)
+        githubPush()
+    }
     definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('jenkins_ami_git_url')  // Replace with your GitHub repository URL
-                        credentials('git-personal-access-token')
-                    }
-                    branch('*/main')
+        cps {
+            script('''
+pipeline {
+    agent any 
+    triggers {
+        // Polling the SCM to detect changes
+        pollSCM('* * * * *') // This configuration polls the SCM every minute (adjust as needed)
+        // GitHub webhook trigger
+        githubPush()
+    }
+    environment {
+        GIT_REPO = 'https://github.com/RamaRaju-vj/static-site.git'
+        GIT_BRANCH = 'main'
+        GIT_CREDENTIALS_ID = 'git-personal-access-token'
+        DOCKER_HUB_CREDENTIALS = credentials('docker-hub-credentials-id')
+    }
+    stages {
+        stage("Clean Workspace") {
+            steps {
+                cleanWs()
+            }
+        }
+
+        stage("Clone static-site repo") {
+            steps {
+                echo "Cloning the repo"
+                withCredentials([usernamePassword(credentialsId: "${GIT_CREDENTIALS_ID}", passwordVariable: 'GIT_TOKEN', usernameVariable: 'GIT_USERNAME')]) {
+                    sh '''
+                        git clone --branch ${GIT_BRANCH} https://${GIT_TOKEN}@github.com/RamaRaju-vj/static-site.git
+                    '''
                 }
             }
-            scriptPath('jenkins/Jenkinsfile')
+        }
+        
+        stage("Build the caddy image and push") {
+            steps {
+                echo "Building the image"
+                withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials-id', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                    sh '''
+                        docker build -t ram2305/caddy:${BUILD_NUMBER} -f static-site/Dockerfile static-site
+                        echo $PASS | docker login -u $USER --password-stdin
+                        docker push ram2305/caddy:${BUILD_NUMBER}
+                    '''
+                }
+            }
+        }
+    }
+}
+            '''.stripIndent())
+            sandbox()
         }
     }
 }
