68af93f0b2f0caa156cced0d3734b0a52b0f82aa
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-07-01 19:59:52 -0400

    fix: script fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -159,13 +159,14 @@ echo "Helm $(helm version)"
 ############################## INSTALL KUBECTL & IAM AUTHENTICATOR ############################
 # Install kubectl and AWS IAM Authenticator
 sudo apt-get update
+cd /usr/local/bin/ || exit 
+
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+
 
 curl -Lo aws-iam-authenticator "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.11/aws-iam-authenticator_0.6.11_linux_amd64"
 sudo chmod +x ./aws-iam-authenticator
-sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 
 echo "Kubectl $(kubectl version --client)"
 echo "AWS IAM Authenticator $(aws-iam-authenticator version)"
