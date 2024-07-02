bee6d8013622ac4275a3c3f4da4cabac5c00a807
RamaRaju-vj <vadapalli.j@northeastern.edu>
2024-07-01 22:46:29 -0400

    fix: install script exec 2

--- scripts/install.sh
+++ scripts/install.sh
@@ -73,7 +73,7 @@ echo "busybox installed."
 # Install AWS CLI
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo /usr/local/bin/unzip awscliv2.zip
-sudo ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+sudo ./aws/install
 echo "AWS CLI installed."
 
 
